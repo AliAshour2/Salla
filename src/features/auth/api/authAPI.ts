247559cf4bc8c559c9202cfa363412d85a967b60
AliAshour2 <aliashour468@gmail.com>
2024-10-02 23:32:39 +0300

    🔧 Removed unnecessary 'async' and 'await' keywords from all methods from AuthApI object  ✅ as they were not needed. Converted to concise arrow functions. 🚀

--- src/features/auth/api/authAPI.ts
+++ src/features/auth/api/authAPI.ts
@@ -3,52 +3,38 @@ import { Base_URL } from "@/constants";
 import { API_URL_AUTH } from "@/constants";
 
 export const AuthAPI = {
-  login: async (credentials: { email: string; password: string }) => {
-    const response = await axios.post(`${API_URL_AUTH}/signin`, credentials);
-    return response.data;
+  login: (credentials: { email: string; password: string }) => {
+    return axios.post(`${API_URL_AUTH}/signin`, credentials);
   },
-  register: async (reqBody: {
+  register: (reqBody: {
     name: string;
     email: string;
     password: string;
     rePassword: string;
     phone: string;
   }) => {
-    const response = await axios.post(`${API_URL_AUTH}/signup`, reqBody);
-    return response.data;
+    return axios.post(`${API_URL_AUTH}/signup`, reqBody);
   },
 
-  forgetPassword : async (email: string) => {
-    const response = await axios.post(`${API_URL_AUTH}/forgotPasswords`, {email});
-    return response.data;
+  forgetPassword: (email: string) => {
+    return axios.post(`${API_URL_AUTH}/forgotPasswords`, { email });
   },
 
-  updateLoggedUserData: async (reqBody: {
+  updateLoggedUserData: (reqBody: {
     name: string;
     email: string;
     phone: string;
-    
   }) => {
     const token = localStorage.getItem("userToken");
-    
-    const response = await axios.put(
-      `${Base_URL}/api/v1/users/updateMe/`,
-      reqBody,
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      }
-    );
-    return response.data;
+
+    return axios.put(`${Base_URL}/api/v1/users/updateMe/`, reqBody, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
   },
 
-
-  verifyResetCode : async (code: string) => {
-    const response = await axios.post(`${API_URL_AUTH}/verifyResetCode`, {code});
-    return response.data;
+  verifyResetCode: async (code: string) => {
+    return axios.post(`${API_URL_AUTH}/verifyResetCode`, { code });
   },
-
-
-
 };
